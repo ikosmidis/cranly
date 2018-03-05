316bf9a048bc4617d9c7fa83ab512082fbbc0e63
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-05 03:49:57 +0000

    Note to see at see http://kateto.net/networks-r-igraph

--- R/coerce.R
+++ R/coerce.R
@@ -2,3 +2,5 @@ as.igraph.cranly_db_network <- function(x, ...) {
     edges <- x$edges
     graph(as.matrix(edges[c("from", "to")])) %>%  set_edge_attr("type", value = edges$type)
 }
+
+## see http://kateto.net/networks-r-igraph for intro
