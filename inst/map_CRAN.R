80aeae52b0de37416f432575c3486ee8f9552cf2
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-14 17:44:00 +0000

    clear CRAN updates

--- inst/map_CRAN.R
+++ inst/map_CRAN.R
@@ -12,9 +12,19 @@
 
 cran_db <- tools::CRAN_package_db()
 packages_db <- clean_CRAN_db(packages_db = cran_db)
-network <- setup_cranly_network(object = packages_db)
+package_network <- setup_cranly_network(object = packages_db)
+author_network <- setup_cranly_network(object = packages_db, perspective = "author")
 
-au_network <- setup_cranly_network(object = packages_db, perspective = "author")
+## Global author network statistics
+author_summaries <- summary(author_network)
+author_summaries %>% arrange(desc(page_rank)) %>% head(10)
+
+## Global package network statistics
+package_summaries <- summary(package_network)
+package_summaries %>% arrange(desc(eigen_centrality)) %>% head(10)
+package_summaries %>% arrange(desc(page_rank)) %>% head(10)
+package_summaries %>% filter(package == "brglm2")
+
 
 my_packages <- subset(network$nodes, grepl("Ioannis Kosmidis", Author))$Package
 
