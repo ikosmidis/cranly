0ad94d22b67f5c89009dec1b699a7fc62d73853d
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-16 13:10:27 +0000

    Updated DESCRIPTION and added global vars

--- R/cranly-package.R
+++ R/cranly-package.R
@@ -8,3 +8,9 @@
 #' @importFrom utils combn stack
 #'
 NULL
+
+if(getRversion() >= "2.15.1")  {
+    utils::globalVariables(c("Author", "Package", "Version", "from", "n_depended_by", "n_depends", "n_enhanced_by", "n_enhances", "n_imported_by", "n_imports", "n_suggested_by", "n_suggests", "to", "type"))
+}
+
+
