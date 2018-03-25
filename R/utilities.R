ade4c2273ce37b1207007baf0b767c4abe0bb568
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-25 18:22:10 +0100

    utilities for new clean_up_author

---
+++ R/utilities.R
@@ -0,0 +1,25 @@
+firstup <- function(x) {
+    stri_trans_totitle(x, opts_brkiter = stri_opts_brkiter(type = "sentence"))
+}
+
+split_and_replace <- function(string, base_patterns, replacement,
+                              nparts = ceiling(length(base_patterns)/5), fixed_word = TRUE) {
+    ns <- length(base_patterns)
+    np <- floor(ns/nparts)
+    u <- c(rep(np, nparts - 1), ns - np * (nparts - 1))
+    parts <- rep(seq.int(nparts), u)
+        if (fixed_word) {
+            out <- lapply(split(base_patterns, parts), function(x) {
+                paste0("(?i)\\b", x, "\\b", collapse = "|")
+            })
+        }
+        else {
+            out <- lapply(split(base_patterns, parts), function(x) {
+                paste0("(?i)", x, collapse = "|")
+            })
+        }
+    for (j in seq_along(out)) {
+        string <- str_replace_all(string, out[[j]], replacement)
+        }
+    string
+}
