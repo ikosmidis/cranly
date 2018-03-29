27d2368a4cf8e025bc0a721a72c7a3e61efb9dba
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-29 02:30:14 +0100

    Added dependency set function

--- R/extractors.R
+++ R/extractors.R
@@ -8,8 +8,8 @@ package_by.cranly_network <- function(x, author = NULL, exact = FALSE) {
     }
     perspective <- attr(x, "perspective")
     if (exact) {
-        str <- paste(author, collapse = "\\b|\\b")
-        str <- paste0("\\b", str, "\\b")
+        str <- paste(author, collapse = "\\b$|^\\b")
+        str <- paste0("^\\b", str, "\\b$")
     }
     else {
         str <- paste(author, collapse = "|")
@@ -32,8 +32,8 @@ author_of.cranly_network <- function(x, package = NULL, exact = FALSE) {
     }
     perspective <- attr(x, "perspective")
     if (exact) {
-        str <- paste(package, collapse = "\\b|\\b")
-        str <- paste0("\\b", str, "\\b")
+        str <- paste(package, collapse = "\\b$|^\\b")
+        str <- paste0("^\\b", str, "\\b$")
     }
     else {
         str <- paste(package, collapse = "|")
@@ -57,8 +57,8 @@ author_with.cranly_network <- function(x, name = NULL, exact = FALSE) {
     }
     perspective <- attr(x, "perspective")
     if (exact) {
-        str <- paste(name, collapse = "\\b|\\b")
-        str <- paste0("\\b", str, "\\b")
+        str <- paste(name, collapse = "\\b$|^\\b")
+        str <- paste0("^\\b", str, "\\b$")
     }
     else {
         str <- paste(name, collapse = "|")
@@ -82,8 +82,8 @@ package_with.cranly_network <- function(x, name = NULL, exact = FALSE) {
     }
     perspective <- attr(x, "perspective")
     if (exact) {
-        str <- paste(name, collapse = "\\b|\\b")
-        str <- paste0("\\b", str, "\\b")
+        str <- paste(name, collapse = "\\b$|^\\b")
+        str <- paste0("^\\b", str, "\\b$")
     }
     else {
         str <- paste(name, collapse = "|")
@@ -99,3 +99,40 @@ package_with.cranly_network <- function(x, name = NULL, exact = FALSE) {
     }
 }
 
+#' @rdname package_by
+#' @export
+imports <- function(x, package = NULL, exact = FALSE) {
+    if (is.null(package)) {
+        return(NULL) # return(unlist(x$nodes$Package))
+    }
+    perspective <- attr(x, "perspective")
+    if (exact) {
+        str <- paste(package, collapse = "\\b$|^\\b")
+        str <- paste0("^\\b", str, "\\b$")
+    }
+    else {
+        str <- paste(package, collapse = "|")
+    }
+    inds <- grep(str, x$nodes$package, ignore.case = !exact)
+    ## inds <- sapply(x$nodes$Package, function(x) any(grepl(str, x)))
+    out <- unique(unlist(x$nodes[inds, "imports"]))
+    if (length(out)) {
+        out
+    }
+    else {
+        NULL
+    }
+}
+
+#' @rdname package_by
+#' @export
+dependency_set <- function(x, package = NULL) {
+    im <- imports(x, package = package, exact = TRUE)
+    im0 <- na.omit(im)
+    if (all(im0 %in% package)) {
+        package
+    }
+    else {
+        unique(c(package, dependency_set(x, package = im0)))
+    }
+}
