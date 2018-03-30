503ee1d8c86aa760e10e48a41526df93260c7b94
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-03-30 07:01:16 +0100

    Updated cran_sample to non-ASCII

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 # Copyright (C) 2018 Ioannis Kosmidis
 
-#' Cleaned and processed CRAN package metadata from 20 March 2018
+#' Cleaned and processed CRAN package metadata from 30 March 2018
 #'
 #' This is a random sample of 1000 R packages from a processed version
 #' of the result of a \code{\link[tools]{CRAN_package_db}()} call on
-#' 20 March 2018. Specifically, the resulting data frame was passed to
+#' 30 March 2018. Specifically, the resulting data frame was passed to
 #' the \code{\link{clean_CRAN_db}} function and only the following
 #' variables have been kept:
 #'
@@ -25,9 +25,13 @@
 #'   \item md5sum
 #' }
 #'
+#' @details
+#'
 #' The author names have been converted to ASCII using transliteration
 #' through the \code{\link{iconv}} function.
 #'
 #' @docType data
 #' @keywords datasets
 "cran_sample"
+
+
