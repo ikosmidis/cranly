e1a19ae9c013dc1645c267a2e7ae3e5ce8ad937c
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-05-15 13:28:35 +0100

    Regex work for R in depends

--- R/clean_CRAN_db.R
+++ R/clean_CRAN_db.R
@@ -135,8 +135,10 @@ clean_up_directives <- function(variable) {
         str_replace_all("\n", ",") %>%
         str_replace_all("\\([^()]*\\)", "") %>%
         str_replace_all(" ", "") %>%
+        ## Eliminate R from dependencies
         str_replace_all("\\bR,\\b", "") %>%
-        str_replace_all("\\bR\\b", "") %>%
+        str_replace_all("\\b,R\\b", "") %>%
+        str_replace_all("^R$", "") %>%
         str_split(",") %>%
         lapply(function(x) {
             out <- str_replace_all(x, ",|^\\s+|\\s+$", "")
