a2dc64414c40e0b6111b97643eeb7b190f7ce1d3
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2022-08-26 13:52:43 +0100

    A cruder approach to data

--- vignettes/update_datasets.R
+++ vignettes/update_datasets.R
@@ -4,6 +4,7 @@ cran_db <- clean_CRAN_db()
 package_network <- build_network(cran_db)
 author_network <- build_network(object = cran_db, perspective = "author")
 
-usethis::use_data(cran_db, overwrite = TRUE)
-usethis::use_data(package_network, overwrite = TRUE)
-usethis::use_data(author_network, overwrite = TRUE)
+save(cran_db, file = "data/cran_db.rda")
+save(package_network, file = "data/package_network.rda")
+save(author_network, file = "data/author_network.rda")
+
