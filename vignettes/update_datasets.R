571ad80eb8af9037ac703c0ba7464bc7bdfe0263
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2022-08-26 13:54:53 +0100

    Trying remote rds

--- vignettes/update_datasets.R
+++ vignettes/update_datasets.R
@@ -4,7 +4,7 @@ cran_db <- clean_CRAN_db()
 package_network <- build_network(cran_db)
 author_network <- build_network(object = cran_db, perspective = "author")
 
-save(cran_db, file = "data/cran_db.rda")
-save(package_network, file = "data/package_network.rda")
-save(author_network, file = "data/author_network.rda")
+saveRDS(cran_db, file = "data/cran_db.rds")
+saveRDS(package_network, file = "data/package_network.rds")
+saveRDS(author_network, file = "data/author_network.rds")
 
